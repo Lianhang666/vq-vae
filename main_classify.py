d60d64d3c074344c082cf7b6988bf3475b1ed7a0
Peiheng Li KTH <145039330+LP-Tobias@users.noreply.github.com>
2024-12-10 23:37:40 +0100

    Update main_classify.py

--- main_classify.py
+++ main_classify.py
@@ -40,7 +40,7 @@ def main():
     for experiment_name in experiment_names:
         for codebook_size in codebook_sizes:
             for model_type in model_types:
-                run = wandb.init(project="Classification_Experiment_1", 
+                run = wandb.init(project="Classification_Experiment_2", 
                                  name=f'{model_type}_{codebook_size}_{experiment_name}',
                                  reinit=True)
                 if model_type == 'vqvae':
